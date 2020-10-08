c62e0cce3a29984d08f8e806039b68f86db4b022
CodeMask <mattode@outlook.com>
2020-10-08 15:35:55 +0100

    api doc

---
+++ envManager/envVariables.py
@@ -0,0 +1,43 @@
+DB_NAME = 'd18lhngcqadte9'
+DB_USER = 'zgkpealnkjigxp'
+DB_PASSWORD = '4e741c733bcbfe4abbf31e80b2b66a0b520d5c7d43439ac0291a696b984e87b8'
+DB_HOST = 'ec2-52-21-247-176.compute-1.amazonaws.com'
+DB_PORT = '5432'
+
+# ROOT_SECRET = 'LC9euClrULxcXghvIAf60VGkSESF6c5U7meVgD4tCEfGakZZH9l67eE7N2a3rFfi0IA6lpynEB0vajaSF8CeSKI3qGUoM2PAMYN780bmabzCESY2Dv3azFeV7suaY4S4cI0MyUxvPtQ97xYlbKxLkVaJ1N2fz2ghGWSwO2EnzPSoc7i3UIr5jATrVWuN90Ui3vO90FbQI4Kgce12GVns8zrWcM0PXHSdim8sSSiPWAvMJ904y2k5F3x93jpOVmR1'
+
+ALLOWED_HOSTS = '172.17.0.1,127.0.0.1,localhost,d2ef4657626c.ngrok.io'
+
+# Django Secret Key
+SECRET_KEY = '(*!q*$^$y3p68uas+kljqvrpwnewzlq%h+h&hcf2)g3&8nj4mb'
+
+# message broker
+RABBIT_MQ_URL = 'amqp://ahspgxqw:8DYljRkjzDzhaNeduJE5uyPvfYH8qxQU@wolverine.rmq.cloudamqp.com/ahspgxqw'
+
+# REDIS
+REDIS_SERVER_NAME = '127.0.0.1'
+REDIS_PORT = '6379'
+
+# SMTP Configuration
+SMTP_HOST = 'smtp-relay.sendinblue.com'
+SMTP_HOST_USER = 'covenant.chukwudi@baseafriquehq.com'
+SMTP_HOST_SENDER = 'info@firshcs.com.ng'
+SMTP_HOST_PASSWORD = 'mHPs6ftJTIQBOWbp'
+SMTP_PORT = '587'
+SMTP_USE_TLS = True
+
+DURATION = '600'
+
+# FIRSHALAL sms config
+FIRSHALAL_BULK_SMS_API_KEY = 'YRsk57Y7el80gMBMPhhsAhfh5wYOa6IjvjYGDuWS3B1cLen3559KW8lP2GUx'
+FIRSHALAL_BULK_SMS_API_URL = 'https://www.bulksmsnigeria.com/api/v1/sms/create'
+FIRSHALAL_BULK_SMS_API_SENDER = 'FIRSHALAL'
+
+# S3 BUCKET CREDENTIALS
+BUCKET_ACCESS_KEY_ID = 'ZEDEAJNDA7D1ZMQPS114'
+BUCKET_SECRET_KEY = 'VgppEq1z9d3kUjszWhTw7QOyi8LeeTzz841uwzFd'
+BUCKET_REGION_NAME = 'eu-central-1'
+BUCKET_NAME = 'lola'
+BUCKET_ENDPOINT_URL = 'https://s3.eu-central-1.wasabisys.com'
+
+PASSWORD_RESET_ENDPOINT = 'auth/reset-password?token='
